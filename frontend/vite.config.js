5e752ff100cae7149d986cbcacb133fca564282e
root <root@ser1ut51kzp1ihh.local>
2026-06-21 08:38:55 +0000

    fix: stack mobile message metadata

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
+    cssMinify: false,
     outDir: '../src/web/public',
     emptyOutDir: true,
     rollupOptions: {
